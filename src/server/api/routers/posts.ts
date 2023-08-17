65ed05095af5308f6307fdc43f3d44dcbf976626
Pawan Paudel <pawanpaudel93@gmail.com>
2023-08-17 10:21:33 +0545

    feat: Add post

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,7 +2,11 @@ import { clerkClient } from "@clerk/nextjs";
 import { type User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -16,6 +20,11 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: [
+        {
+          createdAt: "desc",
+        },
+      ],
     });
 
     const users = (
@@ -42,4 +51,23 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input: { content } }) => {
+      const authorId = ctx.userId;
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content,
+        },
+      });
+
+      return post;
+    }),
 });
